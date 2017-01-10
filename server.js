d104b2adaf83aa68647fba5e2e397af0298cdbaf
Md. Shahriar Anwar <anwarshahriar@gmail.com>
2017-01-10 08:06:35 +0000

    Completed challange

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require("express");
+var multer = require("multer");
+var upload = multer();
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static(__dirname));
+
+app.post('/get-file-size', upload.single('fileToUpload'), function (req, res) {
+    res.send({size: req.file.size});
+});
+
 app.get('/', function (req, res) {
-    res.send('Hello World!'); 
+    res.send('index.html'); 
 });
 
 app.listen(PORT, function () {
